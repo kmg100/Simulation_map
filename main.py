28b20c5d43a7ad8251513e26cf41ba6fa3a108dc
kmg100 <51814761+kmg100@users.noreply.github.com>
2020-12-16 14:28:36 +0200

    Update main.py
    Introduced error for incorrect initialization

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ class PathLoss:
         Parameters
         available modes are static, linear, circular or reset
         """
+        self.stst_mode =False
+        self.lin_mode =False
+        self.circ_mode =False
+        self.tel_mode =False
         self.x1=0#antennas coords
         self.y1=0
         #start_time = time.time()
@@ -30,8 +34,10 @@ class PathLoss:
         self.v:float = v
         if mode == "static":
             print("static mode")
+            self.stst_mode =True
         elif self.mode == "linear":
             print("linear mode")
+            self.lin_mode =True
             self.radians = math.atan2(By-Ay, Bx-Ax)
             self.Vy=v*math.cos(self.radians)
             self.Vx=v*math.sin(self.radians)
@@ -39,12 +45,22 @@ class PathLoss:
             self.newAy = Ay
             self.back = False
         elif self.mode == "circular":
+            self.circ_mode =True
             self.angle = math.radians(90)
             self.Ex = self.Ax + self.Bx + (math.cos(self.angle))#starting coords
             self.Ey = self.Ay + self.By + (math.sin(self.angle))
-        elif self.mode == "reset":
-            print("reset")
+        elif self.mode == "teleport":
+            self.tel_mode =True
+            print("Teleport mode")
             
+        elif self.mode == "reset":
+            self.stst_mode =False
+            self.lin_mode =False
+            self.circ_mode =False
+            self.tel_mode =False
+            print("reset")
+        else:
+            raise Exception("Not valid keyword")
             
             
     def calc_distance(self,x1,y1,x2,y2):
@@ -55,9 +71,12 @@ class PathLoss:
         ##calculats the distance between two antennas
         
     def calc_loss(self,x2,y2):
-        self.calc_distance(self.x1,self.y1,x2,y2)
-        self.pl = 20*(math.log10(self.d))+20*(math.log10(self.fc))+92.45#gigaherz and kilometeres
-        print("The loss is %.2f dB" % self.pl)
+        if self.stst_mode == True:
+            self.calc_distance(self.x1,self.y1,x2,y2)
+            self.pl = 20*(math.log10(self.d))+20*(math.log10(self.fc))+92.45#gigaherz and kilometeres
+            print("The loss is %.2f dB" % self.pl)
+        else:
+            raise Exception("Static mode not initialised")
         return self.pl
     
     def lin_moving(self):#v is speed seit ka parvietojas ar konstantu atrumu no punkta uz punktu un atpakal
@@ -65,42 +84,52 @@ class PathLoss:
         #if self.newAx <= self.Bx and self.newAy <= self.By and self.start == True:
             
             #print("start of road, moving forwords")
-        if self.newAx <= self.Ax and self.newAy <= self.Ay:
-            print("start of road, moving forwords")
-            self.back = False
-        if self.newAx >= self.Bx and self.newAy >= self.By:
-            print("end of road, moving backwords")
-            self.back = True
-        if self.back == True:
-            self.newAx -= self.Vx
-            self.newAy -= self.Vy
-            self.calc_distance(self.x1,self.y1,self.newAx,self.newAy)
-            self.pl = 20*(math.log10(self.d))+20*(math.log10(self.fc))+92.45#gigaherz and kilometeres
-            print("The loss is %.2f dB" % self.pl)
-            time.sleep(1)
+        if self.lin_mode == True:
+            if self.newAx <= self.Ax and self.newAy <= self.Ay:
+                print("start of road, moving forwords")
+                self.back = False
+            if self.newAx >= self.Bx and self.newAy >= self.By:
+                print("end of road, moving backwords")
+                self.back = True
+            if self.back == True:
+                self.newAx -= self.Vx
+                self.newAy -= self.Vy
+                self.calc_distance(self.x1,self.y1,self.newAx,self.newAy)
+                self.pl = 20*(math.log10(self.d))+20*(math.log10(self.fc))+92.45#gigaherz and kilometeres
+                print("The loss is %.2f dB" % self.pl)
+                time.sleep(1)
+            else:
+                self.newAx += self.Vx
+                self.newAy += self.Vy
+                #print(self.newAx)
+                self.calc_distance(self.x1,self.y1,self.newAx,self.newAy)
+                self.pl = 20*(math.log10(self.d))+20*(math.log10(self.fc))+92.45#gigaherz and kilometeres
+                print("The loss is %.2f dB" % self.pl)
+                time.sleep(1)
         else:
-            self.newAx += self.Vx
-            self.newAy += self.Vy
-            #print(self.newAx)
-            self.calc_distance(self.x1,self.y1,self.newAx,self.newAy)
+            raise Exception("Linear mode not initialised")
+    def elip_moving(self):
+        if self.circ_mode == True:
+            self.Ex=self.Ex+self.Bx*self.v*math.cos(self.angle+(math.pi/2))
+            self.Ey=self.Ey+self.By*self.v*math.sin(self.angle+(math.pi/2))
+            self.calc_distance(self.x1,self.y1,self.Ex,self.Ey)
             self.pl = 20*(math.log10(self.d))+20*(math.log10(self.fc))+92.45#gigaherz and kilometeres
             print("The loss is %.2f dB" % self.pl)
+            self.angle += self.v
             time.sleep(1)
-    def elip_moving(self):
-        self.Ex=self.Ex+self.Bx*self.v*math.cos(self.angle+(math.pi/2))
-        self.Ey=self.Ey+self.By*self.v*math.sin(self.angle+(math.pi/2))
-        self.calc_distance(self.x1,self.y1,self.Ex,self.Ey)
-        self.pl = 20*(math.log10(self.d))+20*(math.log10(self.fc))+92.45#gigaherz and kilometeres
-        print("The loss is %.2f dB" % self.pl)
-        self.angle += self.v
-        time.sleep(1)
+        else: 
+            raise Exception("Circular mode not initialised")
+            
     def teleport(self, maxdist,sleeptime):
-        self.Tx = random.randint(0,maxdist)
-        self.Ty = random.randint(0,maxdist)
-        self.calc_distance(self.x1,self.y1,self.Tx,self.Ty)
-        self.pl = 20*(math.log10(self.d))+20*(math.log10(self.fc))+92.45#gigaherz and kilometeres
-        print("The loss is %.2f dB" % self.pl)
-        time.sleep(sleeptime)
+        if self.tel_mode == True:
+            self.Tx = random.randint(0,maxdist)
+            self.Ty = random.randint(0,maxdist)
+            self.calc_distance(self.x1,self.y1,self.Tx,self.Ty)
+            self.pl = 20*(math.log10(self.d))+20*(math.log10(self.fc))+92.45#gigaherz and kilometeres
+            print("The loss is %.2f dB" % self.pl)
+            time.sleep(sleeptime)
+        else: 
+            raise Exception("Teleport mode not initialised")
     def reset(self):
         self.__init__(self.P, self.fc,0, 0, 0, 0, 0, "reset")
 ############Vides izvelesanas
@@ -128,9 +157,9 @@ class PathLoss:
             K = 0
         return K
     
-a = PathLoss(1000.0, 60000, 100, 100,300,100,0.1,mode = "circular")
+a = PathLoss(1000.0, 60000, 100, 100,300,100,0.1,mode = "teleport")
 #a.calc_loss(200, 200)
-for i in range(0,366):
+for i in range(0,10):
     #a.lin_moving()
     #a.elip_moving()
     a.teleport(1000, 1.0)
