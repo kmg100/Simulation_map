283947a3021331bd3910a55740ce54f2d82ea66e
KristapsG <51814761+kmg100@users.noreply.github.com>
2020-12-18 12:33:46 +0200

    Update main.py
    Pievienots grafiks un salabotas funkcijas

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ Created on Tue Dec 15 15:00:42 2020
 
 @author: kristaps
 """
+import matplotlib
+matplotlib.use("TkAgg")
 import math
 import time
+import numpy as np
 import random
-
+from matplotlib import pyplot as plt
 
 class PathLoss:
     def __init__(self, P: float, fc: float, Ox: int, Oy: int, mode: str,**kwargs: any):
@@ -53,7 +56,7 @@ class PathLoss:
         elif self.mode == "linear":
             print("Linear mode with:"+' Frequency-'+str(self.fc)+"GHz"+ ", walking strting coords-"+str(self.Ax)+","+str(self.Ay)+" , end coords-"+str(self.Bx)+","+str(self.By)+" , speed-"+str(self.v))
             self.lin_mode =True
-            self.radians = math.atan2(self.By-self.Ay, self.Bx-self.Ax)
+            self.radians = math.atan2(abs(self.By)-self.Ay, abs(self.Bx)-self.Ax)
             self.Vy=self.v*math.cos(self.radians)
             self.Vx=self.v*math.sin(self.radians)
             self.newAx = self.Ax
@@ -63,8 +66,9 @@ class PathLoss:
             print("Circular mode with:"+'Frequency-'+str(self.fc)+", with the centre at- "+str(self.Ax)+","+str(self.Ay)+",distance in x-"+str(self.Bx)+",distance in y-"+str(self.By)+" , speed-"+str(self.v))
             self.circ_mode =True
             self.angle = math.radians(1)
-            self.Ex = self.Ax + self.Bx + (math.cos(self.angle))#starting coords
-            self.Ey = self.By - self.Ay + (math.sin(self.angle))
+            self.Ex = self.Ax + self.Bx * (math.cos(self.angle))#starting coords
+            self.Ey = self.Ay + self.By * (math.sin(self.angle))
+            print(self.Ax,self.Ay)
         elif self.mode == "teleport":
             self.tel_mode =True
             print("Teleport mode with:"+'Frequency-'+str(self.fc))
@@ -104,7 +108,7 @@ class PathLoss:
     def lin_moving(self):#v is speed seit ka parvietojas ar konstantu atrumu no punkta uz punktu un atpakal
         #when called calculate the new loss
         #if self.newAx <= self.Bx and self.newAy <= self.By and self.start == True:
-            
+        
             #print("start of road, moving forwords")
         if self.lin_mode == True:
             if self.newAx <= self.Ax and self.newAy <= self.Ay:
@@ -120,7 +124,7 @@ class PathLoss:
                 self.calc_distance(self.Ox,self.Oy,self.newAx,self.newAy)
                 self.pl = 10*self.K*(math.log10(self.d))+20*(math.log10(self.fc))+92.45#gigaherz and kilometeres
                 print("The loss is %.2f dB" % self.pl)
-                time.sleep(1)
+                #time.sleep(1)
             else:
                 self.newAx += self.Vx
                 self.newAy += self.Vy
@@ -129,9 +133,11 @@ class PathLoss:
                 self.K = self._calc_K()
                 self.pl = 10*self.K*(math.log10(self.d))+20*(math.log10(self.fc))+92.45#gigaherz and kilometeres
                 print("The loss is %.2f dB" % self.pl)
-                time.sleep(1)
+                #time.sleep(1)
         else:
             raise RuntimeError("Linear mode not initialised")
+        return self.newAx,self.newAy
+            
     def elip_moving(self):
         if self.circ_mode == True:
             self.Ex=self.Ex+self.Bx*self.v*math.cos(self.angle+(math.pi/2))
@@ -141,9 +147,10 @@ class PathLoss:
             self.pl = 10*self.K*(math.log10(self.d))+20*(math.log10(self.fc))+92.45#gigaherz and kilometeres
             print("The loss is %.2f dB" % self.pl)
             self.angle += self.v
-            #time.sleep(1)
+            time.sleep(1)
         else: 
             raise RuntimeError("Circular mode not initialised")
+        return self.Ex,self.Ey
             
     def teleport(self, maxdist,sleeptime):
         if self.tel_mode == True:
@@ -178,11 +185,41 @@ class PathLoss:
             K = 2
         return K
 
+if __name__ == "__main__":
+        
+    a = PathLoss(1000.0, 60, 0,0,Ax = -100, Ay = -100,Bx = -400, By = -400,v = 10,mode = "linear", envo="forest")#nestrada ar negativiem skaitliem
+    """fig = plt.figure()
+    ax1 = fig.add_subplot(1, 1, 1)
+    line, = ax1.plot([], lw=3)
+    fig.canvas.draw()  
+    plt.plot
+    array=[]"""
     
-a = PathLoss(1000.0, 60, 0,0,Ax = 100, Ay = 100,Bx = 300, By = 300,v = 10,mode = "linear", envo="forest")
-#a.calc_loss()
-for i in range(0,50):
-    a.lin_moving()
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 1, 1)
+    x = []
+    y = []
+    ax1.set_xlim(-1000, 1000)
+    ax1.set_ylim(-1000, 1000)
+    line, = ax1.plot([], lw=3)
+    fig.canvas.draw()  
+    plt.show(block=False)
+    #plt.show()#a.calc_loss()
+    for i in range(0,600):
+        ax,ay = a.lin_moving()
+        x.append(ax)
+        y.append(ay)
+        ax1.plot(x, y, '-ok',color='black')
+        fig.canvas.draw()
+        #plt.plot(x, y, '-ok',color='black')
+        #print(ax,ay)
+        #plt.scatter(ax,ay)#
+        fig.canvas.flush_events()
+    #print(x,y)
+    
+
+
+    
 #    a.elip_moving()
     #a.teleport(1000, 1.0)
 #a.reset()
