c50120899a90ca16f5d77a4e1653bcdbf8bbe07d
KristapsG <51814761+kmg100@users.noreply.github.com>
2021-01-07 16:02:31 +0200

    Update CoM.py
    updated to a function

--- CoM.py
+++ CoM.py
@@ -6,22 +6,32 @@ Created on Tue Dec 29 15:09:33 2020
 @author: kristaps
 """
 
-from PIL import Image
+from PIL import Image,ImageDraw
+#from numpy import mgrid, sum
 import numpy as np
 
-im = Image.open('image.bmp')
-immat = im.load()
-(X, Y) = im.size
-m = np.zeros((X, Y))
 
-m = np.sum(np.asarray(im), -1) < 255*3
-m = m / np.sum(np.sum(m))
 
-dx = np.sum(m, 0) # there is a 0 here instead of the 1
-dy = np.sum(m, 1) # as np.asarray switches the axes, because
+def CoM(image): 
+    im = Image.open(image)
+    imm = im.load()
+    (X, Y) = im.size
+    m = np.zeros((X, Y))
+    
+    m = np.sum(np.asarray(im), -1) < 255*3
+    m = m / np.sum(np.sum(m))
+    
+    dx = np.sum(m, 0) 
+    dy = np.sum(m, 1) 
+    
+    # expected values
+    cx = np.sum(dx * np.arange(X))
+    cy = np.sum(dy * np.arange(Y))
+    draw = ImageDraw.Draw(im)
+    draw.text((cx, cy),text="x")
+    im.show()
+    print(cx,cy)
+    return cx,cy
 
-# expected values
-cx = np.sum(dx * np.arange(X))
-cy = np.sum(dy * np.arange(Y))
-im.show()
-print(cx,cy)
+if __name__ == "__main__":
+    CoM("test.png")
